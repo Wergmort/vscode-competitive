79d36b542158dd63838ce8fc03a34ee4e0faf842
kenta misono <kenta14819@gmail.com>
2025-03-30 22:48:44 +0900

    add main

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define yes cout << "yes" << endl;
+#define no cout << "no" << endl;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+template <typename T, typename S>
+auto add(T a, S b) -> decltype(a + b) {
+	return a + b;
+}
+
+int main() {
+	
+}
